75f18e95939a58a5e9a6234c833f71b52e65e503
k1rrra333 <k1rrra0303@gmail.com>
2026-09-17 16:27:22 +0400

    17.09 das

--- script.js
+++ script.js
@@ -1,59 +1,162 @@
-const name = prompt("Enter your name")
-const age = +(prompt("Enter your age"))
-const score = +(prompt("Enter your score"))
-const hasTicket = confirm("Do you have a ticket?")
+// const name = prompt("Enter your name")
+// const age = +(prompt("Enter your age"))
+// const score = +(prompt("Enter your score"))
+// const hasTicket = confirm("Do you have a ticket?")
 
-if (age >= 18) {
-    console.log("You can enter")
-} else {
-    console.log("You are too young")
-}
-
-
-
-console.log("===========================")
+// if (age >= 18) {
+//     console.log("You can enter")
+// } else {
+//     console.log("You are too young")
+// }
 
 
 
-if (age >= 18 && hasTicket === true) {
-    console.log("Entry allowed")
-} else {
-    console.log("Entry denied")
-}
+// console.log("===========================")
 
 
 
-console.log("===========================")
+// if (age >= 18 && hasTicket === true) {
+//     console.log("Entry allowed")
+// } else {
+//     console.log("Entry denied")
+// }
 
 
 
-if (score > 100 || hasTicket === true) {
-    console.log("Special access")
-} else {
-    console.log("Normal access")
-}
+// console.log("===========================")
 
 
 
-console.log("===========================")
+// if (score > 100 || hasTicket === true) {
+//     console.log("Special access")
+// } else {
+//     console.log("Normal access")
+// }
 
 
 
-console.log("Bonus score:", score + 10)
-console.log("Half score:", score / 2)
-console.log("Remainder:", score % 10)
-console.log("Score - 10:", score - 10)
-console.log("Score * 2:", score * 2)
+// console.log("===========================")
 
 
 
-console.log("===========================")
+// console.log("Bonus score:", score + 10)
+// console.log("Half score:", score / 2)
+// console.log("Remainder:", score % 10)
+// console.log("Score - 10:", score - 10)
+// console.log("Score * 2:", score * 2)
 
 
-if (score >= 90) {
-    console.log("Excellent")
-} else if (score >= 60) {
-    console.log("Good")
-} else {
-    console.log("Try again")
-}
+
+// console.log("===========================")
+
+
+// if (score >= 90) {
+//     console.log("Excellent")
+// } else if (score >= 60) {
+//     console.log("Good")
+// } else {
+//     console.log("Try again")
+// }
+
+
+
+
+
+
+// ===================================================
+
+
+
+
+
+
+
+console.log("=============================")
+
+
+const character = "warrior"
+
+console.log(
+    character == "warrior"
+        ? "You chose Warrior"
+        : "Unknown character"
+)
+
+
+console.log("=============================")
+
+
+const hasSword = true
+
+
+console.log(
+    hasSword
+        ? "Warrior is ready"
+        : "Find a weapon"
+)
+
+
+console.log("=============================")
+
+
+const health = 70
+
+console.log(
+    health >= 80
+        ? "Healthy"
+        : health >= 40
+            ? "Wounded"
+            : "Critical"
+)
+
+
+console.log("=============================")
+
+
+const coins = 150;
+
+console.log(
+    coins >= 100
+        ? "You can buy the item"
+        : "Not enough coins"
+)
+
+
+console.log("=============================")
+
+
+const isLoggedIn = true
+const isAdmin = false
+
+console.log(
+    isLoggedIn && isAdmin
+        ? "Admin Panel"
+        : "Access Denied"
+)
+
+
+console.log("=============================")
+
+
+const kills = 12
+const deaths = 3
+
+console.log(
+    kills > deaths
+        ? "Winner"
+        : "Loser"
+)
+
+
+console.log("=============================")
+
+
+const health2 = 85
+const coins2 = 200
+const hasSword2 = true
+
+console.log(
+    health2 >= 50 && coins2 >= 100 && hasSword2 == true
+        ? "READY FOR BATTLE"
+        : "NOT READY"
+)
